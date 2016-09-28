33e9a2d9a6bf47eb0597b6e733b3241ca73aba3a
Gowri Shankar <gowrishankar_m@hotmail.com>
2016-09-28 11:10:51 +0530

    Web view basics:

---
+++ imports/components/todosList/todosList.js
@@ -0,0 +1,51 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import { Tasks } from '../../api/tasks.js';
+import template from './todosList.html';
+
+
+class TodosListCtrl {
+	constructor($scope) {
+		$scope.viewModel(this);
+
+		this.helpers({
+			tasks() {
+				return Tasks.find({}, {
+					sort: {
+						createdAt: -1
+					}
+				});
+			}
+		});
+	}
+
+	addTask(newTask) {
+		Tasks.insert({
+			text: newTask,
+			createdAt: new Date
+		});
+
+		this.newTask = '';
+	}
+
+	setChecked(task) {
+		Tasks.update(task._id, {
+			$set: {
+				checked: !task.checked
+			}
+		});
+	}
+
+	removeTask(task) {
+		Tasks.remove(task._id);
+	}
+}
+
+export default angular.module('todoList', [
+	angularMeteor
+])
+	.component('todosList', {
+		templateUrl: 'imports/components/todosList/todosList.html',
+		controller: ['$scope', TodosListCtrl]
+	});
