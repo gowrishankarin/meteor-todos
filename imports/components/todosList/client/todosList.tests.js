9c80658185006c5d7c9bd2a45d83c0f9b9f6674a
Gowri Shankar <gowrishankar_m@hotmail.com>
2016-09-30 17:07:03 +0530

    Mocha tests

--- imports/components/todosList/client/todosList.tests.js
+++ imports/components/todosList/client/todosList.tests.js
@@ -23,4 +23,37 @@ describe('todosList', function() {
 		element = $compile('<todos-list></todos-list>')($rootScope.$new(true));
 		$rootScope.$digest();
 	});
+
+	describe('component', function() {
+		it('should be showing incomplete tasks count', function() {
+			assert.include(element[0].querySelector('h1').innerHTML, '0');
+		});
+	});
+
+	describe('controller', function() {
+		describe('addTask', function() {
+			var controller;
+			var newTask = 'Be more fabulous';
+
+			beforeEach(() => {
+				sinon.stu(Meteor, 'call');
+				controller = element.controller('todosList');
+				controller.newTask = 'Be fabulous';
+				controller.addTask(newTask);
+			});
+
+			afterEach(() => {
+				Meteor.call.restore();
+			});
+
+			it('should call tasks.insert method', function() {
+				sinon.assert.calledOnce(Meteor.call);
+				sinon.assert.calledWith(Meteor.call, 'tasks.insert', newTask);
+			});
+
+			it('should reset newTask', function() {
+				assert.equal(controller.newTask, '');
+			});
+		});
+	});
 });
